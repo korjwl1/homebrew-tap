efc667132829f52752d36406f264f8d393c6875a
korjwl1 <korjwl1@naver.com>
2026-03-19 14:50:20 +0900

    Initial tap setup for toki

---
+++ Formula/toki.rb
@@ -0,0 +1,36 @@
+class Toki < Formula
+  desc "AI CLI tool token usage tracker"
+  homepage "https://github.com/korjwl1/ddleague-clitrace"
+  version "0.0.0"
+  license "FSL-1.1-Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/korjwl1/ddleague-clitrace/releases/download/v#{version}/toki-#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+    on_intel do
+      url "https://github.com/korjwl1/ddleague-clitrace/releases/download/v#{version}/toki-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/korjwl1/ddleague-clitrace/releases/download/v#{version}/toki-#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+    on_intel do
+      url "https://github.com/korjwl1/ddleague-clitrace/releases/download/v#{version}/toki-#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "toki"
+  end
+
+  test do
+    system "#{bin}/toki", "--version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-tap
+
+Homebrew formulae for [toki](https://github.com/korjwl1/ddleague-clitrace).
+
+## Installation
+
+```bash
+brew tap korjwl1/tap
+brew install toki
+```
+
+> **Note:** While the source repository is private, you need to set a GitHub token:
+>
+> ```bash
+> export HOMEBREW_GITHUB_API_TOKEN=ghp_your_token_here
+> ```
