c73f2f04691e738399a0417ba491a81d130117a7
korjwl1 <korjwl1@naver.com>
2026-03-21 19:31:54 +0900

    Update toki to 1.1.0

--- Formula/toki.rb
+++ Formula/toki.rb
@@ -1,28 +1,28 @@
 class Toki < Formula
   desc "AI CLI tool token usage tracker"
   homepage "https://github.com/korjwl1/toki"
-  version "1.1.0-alpha1"
+  version "1.1.0"
   license "FSL-1.1-Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/korjwl1/homebrew-tap/releases/download/v1.1.0-alpha1/toki-1.1.0-alpha1-aarch64-apple-darwin.tar.gz"
-      sha256 "6553443a6b969b88260c55d8790d38e4071ff4ee276b4d2c7b71587644562cbe"
+      url "https://github.com/korjwl1/homebrew-tap/releases/download/v1.1.0/toki-1.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "fba1cdffbb488bbed3d3da14095f85562dfa3b8b63074ef6bc2d07322dbd5005"
     end
     on_intel do
-      url "https://github.com/korjwl1/homebrew-tap/releases/download/v1.1.0-alpha1/toki-1.1.0-alpha1-x86_64-apple-darwin.tar.gz"
-      sha256 "8b6ab4256629a31becc3d76685ac1c06ed2094d9f622f944b9e4ee21b6494365"
+      url "https://github.com/korjwl1/homebrew-tap/releases/download/v1.1.0/toki-1.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "339325500285541cdddf6abfe0a042f757bcadbb5c686015528834c85111b89a"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/korjwl1/homebrew-tap/releases/download/v1.1.0-alpha1/toki-1.1.0-alpha1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0cc7f536746417c545da5f61b06f4b3d6aa9a66488ce1c4af972889eec90f7df"
+      url "https://github.com/korjwl1/homebrew-tap/releases/download/v1.1.0/toki-1.1.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "a98657a6825e75c466b7e6136a149930831694b40c3b58b1195ca5bc2eda84d1"
     end
     on_intel do
-      url "https://github.com/korjwl1/homebrew-tap/releases/download/v1.1.0-alpha1/toki-1.1.0-alpha1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "256e8e439134a1d08a477177fb5e7be9ad24db72faebd9695cea4353abd41a39"
+      url "https://github.com/korjwl1/homebrew-tap/releases/download/v1.1.0/toki-1.1.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "fd51d68a95db20375efef58b4ef883e93de14d31ce386808a16074179b785200"
     end
   end
 
