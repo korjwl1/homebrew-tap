100eeda01a3dcb25f4b6844d609f757c1df4789f
korjwl1 <korjwl1@naver.com>
2026-03-23 15:33:13 +0900

    Update toki-monitor cask: host binary on homebrew-tap releases

--- Casks/toki-monitor.rb
+++ Casks/toki-monitor.rb
@@ -2,7 +2,7 @@ cask "toki-monitor" do
   version "0.1.0-alpha3"
   sha256 "c0fc9b5a672747b04f6a11d57eda10c650050af54df5a8786bd7f728cdd6ecec"
 
-  url "https://github.com/korjwl1/toki-monitor/releases/download/v#{version}/TokiMonitor-#{version}.zip"
+  url "https://github.com/korjwl1/homebrew-tap/releases/download/toki-monitor-v#{version}/TokiMonitor-#{version}.zip"
   name "Toki Monitor"
   desc "macOS menu bar AI token usage monitor powered by toki TSDB engine"
   homepage "https://github.com/korjwl1/toki-monitor"
