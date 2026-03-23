669a1f0258ac6ecda6b4f153bb1a777bce526eb7
korjwl1 <korjwl1@naver.com>
2026-03-23 15:30:42 +0900

    Update toki-monitor cask to v0.1.0-alpha3

--- Casks/toki-monitor.rb
+++ Casks/toki-monitor.rb
@@ -1,6 +1,6 @@
 cask "toki-monitor" do
-  version "0.1.0-alpha2"
-  sha256 "31665da4a0b58782a30ca3b64571f24b251a22d1fe5a078ea271fc3a11169d0c"
+  version "0.1.0-alpha3"
+  sha256 "c0fc9b5a672747b04f6a11d57eda10c650050af54df5a8786bd7f728cdd6ecec"
 
   url "https://github.com/korjwl1/toki-monitor/releases/download/v#{version}/TokiMonitor-#{version}.zip"
   name "Toki Monitor"
