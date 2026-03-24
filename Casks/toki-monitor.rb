798eda0eb90c5dd98915766100e90d9ed7cf2a21
korjwl1 <korjwl1@naver.com>
2026-03-24 16:18:02 +0900

    toki-monitor: download from toki-monitor repo (now public)

--- Casks/toki-monitor.rb
+++ Casks/toki-monitor.rb
@@ -2,7 +2,7 @@ cask "toki-monitor" do
   version "0.1.2"
   sha256 "0b65fd786cd7e06f4dcb9e3e09cc7dbdf881886ed691b3c4aee1201253484ebb"
 
-  url "https://github.com/korjwl1/homebrew-tap/releases/download/toki-monitor-v#{version}/TokiMonitor-#{version}.zip"
+  url "https://github.com/korjwl1/toki-monitor/releases/download/v#{version}/TokiMonitor-#{version}.zip"
   name "Toki Monitor"
   desc "macOS menu bar AI token usage monitor powered by toki TSDB engine"
   homepage "https://github.com/korjwl1/toki-monitor"
