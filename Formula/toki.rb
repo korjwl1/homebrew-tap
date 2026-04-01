5bb43c0751e28aab5c18e64e66cef8abb0b3da51
korjwl1 <korjwl1@naver.com>
2026-04-02 02:29:38 +0900

    Update toki to 2.0.0

--- Formula/toki.rb
+++ Formula/toki.rb
@@ -1,28 +1,24 @@
 class Toki < Formula
   desc "AI CLI tool token usage tracker"
   homepage "https://github.com/korjwl1/toki"
-  version "1.2.0"
+  version "2.0.0"
   license "FSL-1.1-Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/korjwl1/toki/releases/download/v1.2.0/toki-1.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "e5f41e429d544914e07b051e2261f184d2379873f51671c4f575e7d799e1bfef"
+      url "https://github.com/korjwl1/toki/releases/download/v2.0.0/toki-2.0.0-aarch64-apple-darwin.tar.gz"
+      sha256 "c2d690874ca279f6fb50c9a2ca402b9d66519f3f0ea180b3298c5f8777796ed4"
     end
     on_intel do
-      url "https://github.com/korjwl1/toki/releases/download/v1.2.0/toki-1.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "2c0c37d9353f4547a5122049e5b458e3f54b1b741b1a5f792403635072a73343"
+      url "https://github.com/korjwl1/toki/releases/download/v2.0.0/toki-2.0.0-x86_64-apple-darwin.tar.gz"
+      sha256 "d694748775c87370893c664219a529a47b2b7777bb8b85d7b5974c09d30d111b"
     end
   end
 
   on_linux do
-    on_arm do
-      url "https://github.com/korjwl1/toki/releases/download/v1.2.0/toki-1.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "f871da7cf4af035b4cabbf24fe225d87110777f437513f2e0e940ae977204c4a"
-    end
     on_intel do
-      url "https://github.com/korjwl1/toki/releases/download/v1.2.0/toki-1.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "a40b2d4686c7704b06fe8209e550eeabce770a4503a5f01e5b6bc74f62828238"
+      url "https://github.com/korjwl1/toki/releases/download/v2.0.0/toki-2.0.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "caf2c128af0d31aa19bb77a986538a78a17773e93ee600002e81bbcd477a986d"
     end
   end
 
@@ -37,59 +33,11 @@ class Toki < Formula
       if pid > 0
         begin
           Process.kill(0, pid)
-          ohai "Stopping old toki daemon..."
-          Process.kill("TERM", pid)
-          exited = false
-          10.times do
-            sleep 0.5
-            unless begin; Process.kill(0, pid); true; rescue Errno::ESRCH; false; end
-              exited = true
-              break
-            end
-          end
-          unless exited
-            Process.kill("KILL", pid) rescue nil
-            sleep 0.5
-          end
-          File.delete(pidfile) if File.exist?(pidfile)
-          sock = File.expand_path("~/.config/toki/daemon.sock")
-          File.delete(sock) if File.exist?(sock)
-          sleep 1
-          ohai "Starting toki daemon with new version..."
-          system bin/"toki", "daemon", "start"
+          ohai "Restarting toki daemon to use the new version..."
+          system bin/"toki", "daemon", "restart"
         rescue Errno::ESRCH
-          File.delete(pidfile) if File.exist?(pidfile)
         end
       end
-    end
-  end
-
-  def uninstall
-    pidfile = File.expand_path("~/.config/toki/daemon.pid")
-    if File.exist?(pidfile)
-      pid = File.read(pidfile).strip.to_i
-      if pid > 0
-        begin
-          Process.kill(0, pid)
-          ohai "Stopping toki daemon..."
-          Process.kill("TERM", pid)
-          5.times do
-            sleep 0.5
-            unless begin; Process.kill(0, pid); true; rescue Errno::ESRCH; false; end
-              break
-            end
-          end
-          Process.kill("KILL", pid) rescue nil
-        rescue Errno::ESRCH
-          # already dead
-        end
-      end
-    end
-
-    # Clean up runtime files
-    %w[daemon.pid daemon.sock].each do |f|
-      path = File.expand_path("~/.config/toki/#{f}")
-      File.delete(path) if File.exist?(path)
     end
   end
 
