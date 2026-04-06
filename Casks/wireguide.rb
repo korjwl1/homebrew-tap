2c26f884db40768f0def169e75cfe010fd1c9d5b
korjwl1 <72062804+korjwl1@users.noreply.github.com>
2026-04-06 22:19:11 +0900

    Update wireguide SHA

--- Casks/wireguide.rb
+++ Casks/wireguide.rb
@@ -1,6 +1,6 @@
 cask "wireguide" do
   version "0.1.1"
-  sha256 "c91b70ed0cb3251aada01f740a8eb725d85276d49821c0b9036790d1a8fd87e8"
+  sha256 "b84a3f0b330b82bfa64f77b7ddd3c79a77a3e81b5091e609519206ca90290276"
 
   url "https://github.com/korjwl1/wireguide/releases/download/v#{version}/WireGuide-macOS-arm64.zip"
   name "WireGuide"
