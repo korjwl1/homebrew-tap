155cb5ff14f5e83d6342cfbe0ee018ca1ad64b54
korjwl1 <korjwl1@naver.com>
2026-04-24 17:35:25 +0900

    bump toki-monitor to 0.2.2

--- Casks/toki-monitor.rb
+++ Casks/toki-monitor.rb
@@ -1,6 +1,6 @@
 cask "toki-monitor" do
-  version "0.2.1"
-  sha256 "983acb390ce1b97a92b76a8769a7cb6a0905735b792e0eb9ebf1e0d7b7d6cac9"
+  version "0.2.2"
+  sha256 "b16a43a0baec3e548695311d20d0fb3adf3a0fccef295b113b5e4aa36910a644"
 
   url "https://github.com/korjwl1/toki-monitor/releases/download/v#{version}/TokiMonitor-#{version}.zip"
   name "Toki Monitor"
